72b614e9d6f8a288d1d7769673eda8ba53bdb8f0
hypnoblink <hypnoblink24@gmail.com>
2026-06-22 17:11:59 +0800

    Delete ICCT Journal.bat

--- ICCT Journal.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-:: Navigate to your project folder
-cd /d "C:\Users\Mark Letada\Downloads\My_Stress_App_Project"
-
-:: Run the app using the Python version you installed pygame on (3.13)
-"C:\Users\Mark Letada\AppData\Local\Programs\Python\Python313\python.exe" app.py
-
-pause
